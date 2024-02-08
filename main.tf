98cb73582445afbcb5c567fa30a12107c303721e
Siyana Petkova <106101481+SiyanaPetkova@users.noreply.github.com>
2024-02-08 15:43:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,6 @@ terraform {
       version = "=3.0.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "StorageRG"
-    storage_account_name = "taskboardstoragesp"
-    container_name       = "taskboardcontainer"
-    key                  = "terraform.tfstate"
-  }
 }
 
 
